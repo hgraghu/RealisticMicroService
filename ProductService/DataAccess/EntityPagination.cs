0b946b0d47b8400420ea5855f42cca4146a69b45
Raghu H G <hgraghu@gmail.com>
2024-06-15 12:40:39 +0530

    test

--- ProductService/DataAccess/EntityPagination.cs
+++ ProductService/DataAccess/EntityPagination.cs
@@ -3,6 +3,9 @@
     public class EntityPagination
     {
         public int PageIndex { get; set; } = 1;
+
+
+
         public int PageSize { get; set; } = 10;
         public string SortField { get; set; } = "Id";
         public string SortOrder { get; set; } = "asc";
